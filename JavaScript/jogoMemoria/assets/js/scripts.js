66dd20e0915dd8f6525cb8d202abae19e3e566dd
Arci Borgmann <arci.borgmann@hotmail.com>
2022-07-25 18:02:02 -0300

    Jogo da Memória

---
+++ JavaScript/jogoMemoria/assets/js/scripts.js
@@ -0,0 +1,60 @@
+const CARDS = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
+
+function flipCard() {
+    if(lockBoard) return;
+    if(this === firstCard) return;
+    this.classList.add('flip');
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMatch();
+}
+
+function checkForMatch() {
+    if (firstCard.dataset.card === secondCard.dataset.card) {
+        disableCards();
+        return;
+    }
+    unFlipCards();
+}
+
+function unFlipCards() {
+    lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        resetBoard();
+    }, 1500);
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    resetBoard();
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [false, false]
+}
+
+(function shuffleBoard() {
+    CARDS.forEach((card) => {
+        let randomPosition = Math.floor(Math.random() * 12);
+        card.style.order = randomPosition;
+    })
+})();
+
+CARDS.forEach((card) => {
+    card.addEventListener('click', flipCard)
+});
+
+
